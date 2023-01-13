39a4339c14d85a0fd030b1bd197e47cd6a3ceca2
marcoagbarreto <6492392+marcoagbarreto@users.noreply.github.com>
2023-01-13 15:27:31 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# spotify-ad-mute
+This is a program to mute spotify whenever an ad appears.
+
+[//]: # (* Download the latest release of [spotify-ad-mute.exe]&#40;https://github.com/marcoagbarreto/pressingKeys/releases/download/v0.2.4/pressingKeys.exe&#41;.)
+
+## Example
+
+1. Run ```spotify-ad-mute.exe``` or ```spotify-ad-mute.py```
+2. Select ```Run``` using the arrow keys and press ```Enter ```
+3. Profit.
+4. Select ```Stop``` to stop the program.
+
+![example](example.gif)
+
+## Code Usage
+
+clone the repository (no installation required, source files are sufficient):
+        
+    https://github.com/marcoagbarreto/pressingKeys.git
+
+dependencies:
+
+    import os
+    import time
+    import pyautogui
+    from threading import Thread
+    from pick import pick
+
+[//]: # (or [download and extract the zip]&#40;https://github.com/marcoagbarreto/pressingKeys/archive/main.zip&#41; into your project folder.)
+
+## Known limitations:
+* Spotify must be on the main screen and always visible.
---
+++ spotify-ad-mute.py
@@ -0,0 +1,90 @@
+"""
+Title: spotify-ad-mute
+Description: This is a program to mute spotify whenever an ad appears.
+Author: Marco A. Barreto - marcoagbarreto
+Version: 13-Jan-2023
+"""
+
+try:
+    import os
+    import sys
+    from pyautogui import locateCenterOnScreen as pyautogui_locateCenterOnScreen, click as pyautogui_click
+    import time
+    from pick import pick
+    from threading import Thread as threading_Thread
+except ImportError as details:
+    print("-E- Couldn't import module, try pip install 'module'")
+    raise details
+
+
+# function needed for auto-py-to-exe to find relative path when one-file is used.
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
+
+
+image_volume = resource_path('img/volume.png')
+image_mute = resource_path('img/mute.png')
+image_ad = [resource_path('img/en.png'), resource_path('img/es.png')]
+
+
+class MuteAd(threading_Thread):
+    def __init__(self):
+        super(MuteAd, self).__init__()
+        self.program_running = True
+        self.running = False
+
+    def run(self):
+        while self.program_running:
+            while self.running:
+                xy_ad = check_for_ad(image_ad)
+                if xy_ad is not None:
+                    xy_volume = locate_image(image_volume)
+                    if xy_volume is not None:
+                        pyautogui_click(x=xy_volume[0], y=xy_volume[1], clicks=1, button='left')
+                    time.sleep(0.5)
+                else:
+                    xy_mute = locate_image(image_mute)
+                    if xy_mute is not None:
+                        pyautogui_click(x=xy_mute[0], y=xy_mute[1], clicks=1, button='left')
+                    time.sleep(0.5)
+
+
+def locate_image(image):
+    return pyautogui_locateCenterOnScreen(image, grayscale=True, confidence=0.9)
+
+
+def check_for_ad(ads):
+    for image in ads:
+        xy_ad = locate_image(image)
+        if xy_ad is not None:
+            return xy_ad
+
+
+def show_menu(mute_ad):
+    title = f'spotify-ad-mute: (({mute_ad.running}))'
+    options = ['Run', 'Stop']
+    option, index = pick(options, title)
+
+    if index == 0:
+        mute_ad.running = True
+    else:
+        mute_ad.running = False
+
+
+def main():
+    # Set terminal size to optimal size
+    os.system('mode con cols=30 lines=7')
+
+    # Start program
+    mute_ad = MuteAd()
+    mute_ad.start()
+
+    while True:
+        # show menu
+        show_menu(mute_ad)
+
+
+if __name__ == "__main__":
+    main()
